efd703f90d6ee61ae58a7ce80e80107de1270836
Tyler B Yeager <wow_d2@hotmail.com>
2021-11-30 14:18:39 -0800

    added other required dependencies. Attempting to bug fix

---
+++ client/build/precache-manifest.53dc3e94af15ab2c5180410c71a3f23d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5ca160790b0df8bc1be0e2345bf738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4279b7beb43b04a716c",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "b495b1b0d430770ae113",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "b4279b7beb43b04a716c",
+    "url": "/static/js/2.3534d3f2.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.3534d3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b495b1b0d430770ae113",
+    "url": "/static/js/main.62025ab2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
